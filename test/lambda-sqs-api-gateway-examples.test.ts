0c2bb3da4da93a9dc8eb6b5e05574f79149eb7d5
nakajima97 <kituneudon97@gmail.com>
2023-06-19 22:20:30 +0900

    Initial commit

---
+++ test/lambda-sqs-api-gateway-examples.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LambdaSqsApiGatewayExamples from '../lib/lambda-sqs-api-gateway-examples-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/lambda-sqs-api-gateway-examples-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LambdaSqsApiGatewayExamples.LambdaSqsApiGatewayExamplesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
