970620b276500c69695f074cdcd9a941e82becad
Yudai Nakajima <kituneudon97@gmail.com>
2023-07-01 16:07:08 +0900

    #3 api gatewayとSQSを統合

--- lib/lambda-sqs-api-gateway-examples-stack.ts
+++ lib/lambda-sqs-api-gateway-examples-stack.ts
@@ -3,8 +3,8 @@ import { Construct } from 'constructs';
 import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
 import { Queue } from 'aws-cdk-lib/aws-sqs';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { AwsIntegration, LambdaRestApi, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { PolicyStatement, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
 
 export class LambdaSqsApiGatewayExamplesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,17 +12,6 @@ export class LambdaSqsApiGatewayExamplesStack extends cdk.Stack {
 
     // Create the SQS queue
     const queue = new Queue(this, 'Queue');
-
-    // Create the Lambda function for pushing data to SQS
-    const pushDataToQueueFunction = new Function(this, 'PushDataToQueueFunction', {
-      runtime: Runtime.NODEJS_14_X,
-      handler: 'index.handler',
-      code: Code.fromAsset('lambda/pushDataToQueueFunction'),
-      environment: {
-        QUEUE_URL: queue.queueUrl
-      },
-      timeout: cdk.Duration.seconds(10),
-    });
 
     // Create the Lambda function for processing data from SQS
     const processDataFromQueueFunction = new Function(this, 'ProcessDataFromQueueFunction', {
@@ -36,11 +25,53 @@ export class LambdaSqsApiGatewayExamplesStack extends cdk.Stack {
     });
     processDataFromQueueFunction.addEventSource(new SqsEventSource(queue));
 
-    // Create the API Gateway
-    new LambdaRestApi(this, 'Endpoint', {
-      handler: pushDataToQueueFunction
+    const integrationRole = new Role(this, 'integration-role', {
+      assumedBy: new ServicePrincipal('apigateway.amazonaws.com')
     });
 
-    pushDataToQueueFunction.addToRolePolicy(new PolicyStatement({ actions: ['sqs:SendMessage'], resources: [queue.queueArn] }))
+    queue.grantSendMessages(integrationRole);
+
+    const sendMessageIntegration = new AwsIntegration({
+      service: 'sqs',
+      path: `${process.env.CDK_DEFAULT_ACCOUNT}/${queue.queueName}`,
+      integrationHttpMethod: 'POST',
+      options: {
+        credentialsRole: integrationRole,
+        requestParameters: {
+          'integration.request.header.Content-Type': `'application/x-www-form-urlencoded'`
+        },
+        requestTemplates: {
+          'application/json': 'Action=SendMessage&MessageBody=$input.body'
+        },
+        integrationResponses: [
+          {
+            statusCode: '200'
+          },
+          {
+            statusCode: '400'
+          },
+          {
+            statusCode: '500'
+          }
+        ]
+      }
+    });
+
+    // Rest API
+    const api = new RestApi(this, 'api', {});
+
+    api.root.addMethod('POST', sendMessageIntegration, {
+      methodResponses: [
+        {
+          statusCode: '400'
+        },
+        {
+          statusCode: '200'
+        },
+        {
+          statusCode: '500'
+        }
+      ]
+    })
   }
 }
